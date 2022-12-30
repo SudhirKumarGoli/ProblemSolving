6859dce2f898cd06c0e31cb52d7e777180e1718b
Sudhir Kumar <113962729+SudhirKumarGoli@users.noreply.github.com>
2022-12-31 00:46:56 +0530

    Create colorfulNumber.java
    Given a number A, find if it is COLORFUL number or not.
    
    If number A is a COLORFUL number return 1 else, return 0.
    
    What is a COLORFUL Number:
    
    A number can be broken into different contiguous sub-subsequence parts. 
    Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
    And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different.

---
+++ colorfulNumber.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class prod {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(colorful(a));
+    }
+    public static int colorful(int A) {
+        // Convert the number to a string
+        String str = String.valueOf(A);
+        // Initialize a set to store the products
+        Map<Integer, Integer> products = new HashMap<>();
+
+        // Iterate through the string
+        for (int i = 0; i < str.length(); i++) {
+            // Get all the contiguous subsequences that contain the current character
+            for (int j = i; j < str.length(); j++) {
+                // Calculate the product of the digits in the contiguous subsequence
+                int product = 1;
+                for (int k = i; k <= j; k++) {
+                    product *= Character.getNumericValue(str.charAt(k));
+                }
+                // Check if the product is already in the set
+                if (products.containsKey(product)) {
+                    return 0; // Not a COLORFUL number
+                }
+                // Add the product to the set
+                products.put(product, 1);
+            }
+        }
+        return 1; // COLORFUL number
+    }
+
+}
