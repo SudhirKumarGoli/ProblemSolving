b05c30782bfb5c6c116064f2f396a6115c750b82
Sudhir Kumar <113962729+SudhirKumarGoli@users.noreply.github.com>
2023-01-06 00:39:39 +0530

    Update BeggersOutsideTemple.java

--- BeggersOutsideTemple.java
+++ BeggersOutsideTemple.java
@@ -1,14 +1,34 @@
-int[] ans = new int [b.length];
-for(int i=0;i<B.length; i++){
-      int l=B[ i ][ 0 ]-1
-      int r=B[ i ][ 1 ]-1
-      int val=B[ i ][ 2 ];
-      ans[ l ]+=val;
-      if(r+1<ans.length){
-           ans[r+1]-=val
+public class Sudhir {
+      
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+              int A = 5;
+              int [][]B = new int [3][3];
+             //[[1, 2, 10], [2, 3, 20], [2, 5, 25]]
+              for(int i =0; i<B.length; i++){
+                    for(int j =0; j<B[0].length; j++){
+                          B[i][j] = sc.nextInt();
+                    }
+              }
+              System.out.println(solve(A,B));
       }
+      
+    public static int[] solve(int A, int[][] B) {
+      int[] ans = new int [B.length];
+      for(int i=0;i<B.length; i++){
+            int l=B[i][0]-1;
+            int r=B[i][1]-1;
+            int val=B[i][2];
+            ans[l]+=val;
+            if(r+1<ans.length){
+                 ans[r+1]-=val;
+            }
+      }
+          
+      for(int i=1;i<ans.length;i++){
+          ans[i]=ans[i-1]+ans[i]
+      }
+
+            return ans;
+   }
 }
-for(int i=1;i<ans.len;i++){
-    ans[i]=ans[i-1]+ans[i]
-}
-return ans;
