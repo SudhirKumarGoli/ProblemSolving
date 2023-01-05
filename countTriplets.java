359238d8c66174c226c1644237df57008283d8ec
Sudhir Kumar <113962729+SudhirKumarGoli@users.noreply.github.com>
2023-01-06 00:03:54 +0530

    Count Increasing Triplets
    You are given an array A of N elements. Find the number of triplets i,j and k such that i<j<k and A[i]<A[j]<A[k].
    
    Example Input:
        Input 1:
        
        A = [1, 2, 4, 3]
        Input 2:
        A = [2, 1, 2, 3]
    
        Example Output:
        
        Output 1:
        2
        Output 2:
        1
    
        Example Explanation:
        
        For Input 1:
        The triplets that satisfy the conditions are [1, 2, 3] and [1, 2, 4].
        For Input 2:
        The triplet that satisfy the conditions is [1, 2, 3].

--- countTriplets.java
+++ countTriplets.java
@@ -1,27 +1,4 @@
-/*    Example Input:
-    Input 1:
-    
-    A = [1, 2, 4, 3]
-    Input 2:
-    A = [2, 1, 2, 3]
-
-    Example Output:
-    
-    Output 1:
-    2
-    Output 2:
-    1
-
-    Example Explanation:
-    
-    For Input 1:
-    The triplets that satisfy the conditions are [1, 2, 3] and [1, 2, 4].
-    For Input 2:
-    The triplet that satisfy the conditions is [1, 2, 3].
-    
-*/
-
-    public class Sudhir {
+public class Sudhir {
         public static void main(String[] args) {
             int[] arr = {1, 2, 4, 3};
             System.out.print(solve(arr));
